4c13fea100ee81b34e66ef15dfdc6e2882515d25
nananina <nananina0415@gmail.com>
2026-01-06 02:13:42 +0900

    새롭게 시작

--- windows-connect.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM RCCR Windows Auto-Connect Wrapper
-REM Double-click this file to auto-connect to Control node
-
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0windows-connect.ps1"
-pause
